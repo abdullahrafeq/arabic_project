bb911a233b1c6592bf7eefc33fc99b609dd88472
Abdullah Rafeq <abdullah-rafeq@hotmail.com>
2025-01-07 00:01:16 +0100

    .

--- client/src/contexts/BaseUrlContext.js
+++ client/src/contexts/BaseUrlContext.js
@@ -3,7 +3,9 @@ import React, { createContext, useContext } from 'react';
 const BaseUrlContext = createContext();
 
 export const BaseUrlProvider = ({ children }) => {
-  const BASE_URL = process.env.REACT_APP_BACKEND_URL;
+  const apiUrl = /choreo-apis/arabicexplorerproject/server/v1
+
+  const BASE_URL = process.env.REACT_APP_BACKEND_URL ? process.env.REACT_APP_BACKEND_URL : apiUrl;
 
   return (
     <BaseUrlContext.Provider value={BASE_URL}>
